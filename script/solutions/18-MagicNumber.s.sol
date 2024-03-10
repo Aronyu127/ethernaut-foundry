ed96cb5c3edebf8bd783dcbe1527f3c7726c5334
aronyu <h0935674938@hotmail.com>
2024-03-10 18:03:37 +0800

    18-MagicNumber

--- script/solutions/18-MagicNumber.s.sol
+++ script/solutions/18-MagicNumber.s.sol
@@ -5,7 +5,6 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
-//  import "../../src/18-MagicNumberAttacker.sol";
 
 contract MagicNumberSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0x2132C7bc11De7A90B87375f282d36100a29f97a9;
@@ -17,6 +16,13 @@ contract MagicNumberSolution is Script, EthernautHelper {
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
         // YOUR SOLUTION HERE
+        bytes memory code = "\x69\x60\x2a\x60\x00\x52\x60\x20\x60\x00\xf3\x60\x00\x52\x60\x0a\x60\x16\xf3";
+        address solver;
+        assembly {
+            solver := create(0, add(code, 0x20), mload(code))
+        }
+
+        challengeInstance.call(abi.encodeWithSignature("setSolver(address)", address(solver)));
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
         bool levelSuccess = submitInstance(challengeInstance);
